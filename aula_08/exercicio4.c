97474a86b998ef1f7282b1eb59bc32820e87567a
viniciusrn020806 <vinicius020806@gmail.com>
2025-09-26 20:38:47 -0300

    .

--- aula_08/exercicio4.c
+++ aula_08/exercicio4.c
@@ -3,18 +3,43 @@
 
 void formatar_nome(char * nome)
 {   
-    int i = 0;
+    int i = 0, j = 0;
     while(nome[i] == ' ')
     {
         nome[i] = nome[i + 1];
         i++;
     }
+    while(nome[j] != '\0')
+    {
+        nome[j] = nome[i];
+        j++;
+        i++;
+    }
+    nome[j] = '\0';
+
+    i = 0;
+    while(nome[i] != ' ')
+    { 
+        i++;
+    }
+    j = i + 1; 
+    while(nome[j] == ' ' )
+    {
+        j++;
+    }
+    while(nome[j] != '\0')
+    {
+        nome[i + 1] = nome[j];
+        i++;
+        j++;
+    }
+    nome[i + 1] = '\0';
 }
 
 
 int main()
 {
-   char nome[]= "    Luciano";
+   char nome[]= "    Luciano             Rossi  ";
    formatar_nome(nome);
    printf("%s", nome);
     return 0;
